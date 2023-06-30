d60a449d2e92979eaa326dddc438490c2c1988cf
ModalHydra <geb190000@utdallas.edu>
2023-06-29 19:16:33 -0500

    started working on task component

--- GeneralTaskManagerApplication/my-app/src/Components/taskManagerComponents/Task.js
+++ GeneralTaskManagerApplication/my-app/src/Components/taskManagerComponents/Task.js
@@ -2,8 +2,62 @@ export default function Task(){
 
     return(
         <>
-        
-        
+            <style>
+            {`
+                #taskItem{
+
+                    background: green;
+                    width: 80%;
+                    display: table-row;
+                }
+
+                .taskCell{
+                    background: #FF8585;
+                    width: 20%;
+                    display: table-cell;
+                    border: solid 1px;
+                    margin: auto;
+                }
+
+                
+
+                
+                }
+            
+            `}
+            </style>
+
+            <div id = "taskItem">
+
+                <div id = "taskDate" class = "taskCell">
+                    date
+                </div>
+                <div id = "taskName" class = "taskCell">
+                    task name
+                </div>
+
+                <div class = "taskCell">
+                    <button id = "confirmButton">
+                        confirm
+                    </button>
+
+                </div>
+
+                <div class = "taskCell">
+                    <button id = "skipButton">
+                        skip
+                    </button>
+                </div>
+
+                <div class = "taskCell">
+                    <button id = "deleteButton">
+                        delete
+                    </button>
+                </div>
+
+
+            </div>
+
         </>
     );
 }
--- GeneralTaskManagerApplication/my-app/src/Pages/taskManagerPages/taskManagerHome.js
+++ GeneralTaskManagerApplication/my-app/src/Pages/taskManagerPages/taskManagerHome.js
@@ -1,4 +1,5 @@
 import SideBar from "../../Components/taskManagerComponents/SideBar";
+import Task from "../../Components/taskManagerComponents/Task";
 
 export default function TaskManagerHomer(){
 
@@ -8,9 +9,28 @@ export default function TaskManagerHomer(){
                 {`
                     #homeTitle{
                         width: 85%;
-                        font-size: 90pt;
+                        font-size: 70pt;
                         text-align: center;
                         float: right;
+                    }
+
+                    #taskHolder{
+                        background: #a0a096;
+                        width: 70%;
+                        height: 60vh;
+                        float: right;
+                        text-align: center;
+                        position: absolute;
+                        top: 25%;
+                        left: 22%;
+                    }
+
+                    #taskList{
+                        
+                    }
+
+                    li.task{
+                        margin: 5px;
                     }
                 
                 
@@ -21,7 +41,28 @@ export default function TaskManagerHomer(){
              <SideBar/>
              <div id = "homeTitle">
                     To Do...
+                    <br/>
              </div>
+             <div id = "taskHolder">
+                <ol id = "taskList">
+                    <li class = "task">
+                        <Task/>
+                    </li>
+                    <li class = "task">
+                        <Task/>
+                    </li>
+                    <li class = "task">
+                        <Task/>
+                    </li>
+                    <li class = "task">
+                        <Task/>
+                    </li>
+                    <li class = "task">
+                        <Task/>
+                    </li>
+                </ol>
+             </div>
+            
              
         </div>
     );
